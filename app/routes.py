9da4e628260107dfb1fe5221a5a44249977fd6cc
DanielAvilez1 <104475313+DanielAvilez1@users.noreply.github.com>
2022-08-02 14:43:45 -0700

    class 1

---
+++ app/routes.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello World </h1>"
+
+
+@app.route("/about")
+def about():
+    me = {
+        "first_name": "Daniel",
+        "last_name": "Avilez",
+        "hobby": "Collecting shoes"
+
+
+    }
+    return me
